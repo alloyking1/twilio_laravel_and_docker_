22bf3a5d1b233091da07cf56d31250848d2301ac
alloyking1 <alloyking1@gmail.com>
2023-08-21 15:19:25 +0100

    fixed bug in the entrypoint.sh file

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if[ ! -f "vendor/autoload.php"]; then
+if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
-if[! -f ".env"]; then
+if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
     cp .env.example .env
 else
-    echo "env file exist"
+    echo "env file exist."
 fi
 
 php artisan migrate
